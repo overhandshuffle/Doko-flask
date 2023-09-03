cfedb8043a11daf9d242962726a3891d0c9457d0
overhandshuffle <malterothkamm@mail.com>
2023-09-03 09:49:35 +0200

    Dockerinit

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from doppelkopf import app
 
 
 if __name__ == "__main__":
-    #app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True)
     
-    from doppelkopf import groups
-    groups.groups_all(8)
+    #from doppelkopf import groups
+    #groups.groups_all(8)
